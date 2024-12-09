ac288034d2090b92454a8f13986c1d4833ec444f
perpel <572821520@qq.com>
2024-12-09 11:31:05 +0800

    mm

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kordar/eventbus
 
 go 1.16
+
+require (
+	github.com/kordar/gologger v0.0.8
+	github.com/spf13/cast v1.7.0
+)
--- starter.go
+++ starter.go
@@ -1,1 +1,51 @@
 package eventbus
+
+import (
+	"github.com/kordar/gologger"
+	"github.com/spf13/cast"
+)
+
+type EventBusModule struct {
+	name string
+	load func(moduleName string, itemId string, item map[string]string, eventBus *EventBus)
+}
+
+func NewEventBusModule(name string, load func(moduleName string, itemId string, item map[string]string, eventBus *EventBus)) *EventBusModule {
+	return &EventBusModule{name, load}
+}
+
+func (m EventBusModule) Name() string {
+	return m.name
+}
+
+func (m EventBusModule) _load(id string, cfg map[string]string) {
+
+	if id == "" {
+		logger.Fatalf("[%s] the attribute id cannot be empty.", m.Name())
+		return
+	}
+
+	eventBus := NewEventBus()
+	if m.load != nil {
+		m.load(m.name, id, cfg, eventBus)
+		logger.Debugf("[%s] triggering custom loader completion", m.Name())
+	}
+
+	logger.Infof("[%s] loading module '%s' successfully", m.Name(), id)
+}
+
+func (m EventBusModule) Load(value interface{}) {
+	items := cast.ToStringMap(value)
+	if items["id"] != nil {
+		id := cast.ToString(items["id"])
+		m._load(id, cast.ToStringMapString(value))
+		return
+	}
+
+	for key, item := range items {
+		m._load(key, cast.ToStringMapString(item))
+	}
+}
+
+func (m EventBusModule) Close() {
+}
